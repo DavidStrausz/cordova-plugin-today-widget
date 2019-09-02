ee48f1cc1817c060d8eea3eab5413d5962c46e8f
Remy Kabel <remy.kabel@olisto.com>
2019-09-02 12:49:58 +0200

    add prefix

--- hooks/copyExtensionFolderToIosProjectFolder.js
+++ hooks/copyExtensionFolderToIosProjectFolder.js
@@ -48,7 +48,7 @@ log(
 );
 
 // http://stackoverflow.com/a/26038979/5930772
-var copyFileSync = function(source, target) {
+var copyFileSync = function(source, target, prefix) {
   var targetFile = target;
 
   // If target is a directory a new file with the same name will be created
@@ -58,7 +58,12 @@ var copyFileSync = function(source, target) {
     }
   }
 
-  fs.writeFileSync(targetFile, fs.readFileSync(source));
+  if (ref) {
+    var prefixedTarget = path.join(path.dirname(targetFile), prefix + '-' + path.basename(targetFile));
+    fs.writeFileSync(prefixedTarget, fs.readFileSync(source));
+  } else {
+    fs.writeFileSync(targetFile, fs.readFileSync(source));
+  }
 };
 var copyFolderRecursiveSync = function(source, target) {
   var files = [];
@@ -170,7 +175,7 @@ module.exports = function(context) {
 
         var destFolder = path.join(iosFolder, widgetName);
 
-        copyFileSync(srcFile, destFolder);
+        copyFileSync(srcFile, destFolder, widgetName);
       });
       log('Successfully copied dependency files!', 'success');
     }
